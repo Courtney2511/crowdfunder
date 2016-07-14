e46772e425246fb8665339e1e27bdf89fb66f105
Ji You <kroyagis@gmail.com>
2016-07-14 14:52:01 -0400

    create and new method for pledge controller

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,22 @@
 class PledgesController < ApplicationController
+
+  def new
+    @pledge = Pledge.new
+  end
+
+  def create
+    @pledge = @project.pledges.build(pledge_params)
+    @pledge.user = current_user
+    if @pledge.save
+      redirect_to projects_url(@project)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def pledge_params
+    params.require(:pledge).permit(:amount)
+  end
 end
