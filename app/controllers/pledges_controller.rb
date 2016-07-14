f610b128ec6eda76c4e099b3c49472f4ba89878d
Ji You <kroyagis@gmail.com>
2016-07-14 15:55:44 -0400

    added before_action load project

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,4 +1,6 @@
 class PledgesController < ApplicationController
+  before_action :load_project
+  # before_action :ensure_logged_in, only: [:create, :destroy]
 
   def new
     @pledge = Pledge.new
@@ -18,5 +20,9 @@ class PledgesController < ApplicationController
   private
   def pledge_params
     params.require(:pledge).permit(:amount, :reward_id)
+  end
+
+  def load_project
+    @project = Project.find(params[:project_id])
   end
 end
