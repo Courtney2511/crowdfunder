d421e873c74e26e2f98dda98268b9d61004ea1df
Ji You <kroyagis@gmail.com>
2016-07-15 06:51:19 -0400

    seed fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 
 Project.create(name: 'Crowdfunder', description: 'Let\'s crowdfund!', goal: 10000, user_id: 1, deadline: (Date.today + 12.months))
 
-Project.create(name: 'Crowdfunder2', description: 'Let\'s crowdfund!', goal: 10000, user_id: 1, deadline: (Date.today + 12.months))
+Project.create(name: 'RPG Funder', description: 'It\'s all about the Role Play', goal: 200000, user_id: 2, deadline: (Date.today + 18.months))
 
 Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.first.id, quantity: 3)
 Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.first.id, quantity: 3)
 Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.first.id, quantity: 3)
 
-Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.second.id, quantity: 3)
-Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.second.id, quantity: 3)
-Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.second.id, quantity: 3)
+Reward.create(name: 'Sticky mouse', description: 'It\'s ultra sticky!!', amount: '1000', project_id: Project.second.id, quantity: 3)
+Reward.create(name: 'Sticky keyboard', description: 'It\'s ultra sticky!!', amount: '1000', project_id: Project.second.id, quantity: 3)
+Reward.create(name: 'Sticky t-shirt', description: 'It\'s ultra sticky!!', amount: '1000', project_id: Project.second.id, quantity: 3)
