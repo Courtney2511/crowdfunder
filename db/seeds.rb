1c5c1507a61c8c3b7ec961f55036193d2bc4b05c
Ji You <kroyagis@gmail.com>
2016-07-15 06:28:25 -0400

    added profile link and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Project.create(name: 'Crowdfunder', description: 'Let\'s crowdfund!', goal: 10000, user_id: 1, deadline: (Date.today + 12.months))
+
+Project.create(name: 'Crowdfunder2', description: 'Let\'s crowdfund!', goal: 10000, user_id: 1, deadline: (Date.today + 12.months))
+
+Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.first.id, quantity: 3)
+Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.first.id, quantity: 3)
+Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.first.id, quantity: 3)
+
+Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.second.id, quantity: 3)
+Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.second.id, quantity: 3)
+Reward.create(name: 'Leg Shaver', description: 'It\'s ultra sharp!', amount: '1000', project_id: Project.second.id, quantity: 3)
